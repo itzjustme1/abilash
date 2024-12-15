0bd0d4bbbad364eee832fec1924be1c7f020607d
vbdgrijff <151443637+vbdegrijff@users.noreply.github.com>
2024-12-15 22:29:23 +0100

    Update ProfileImage.tsx

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -5,7 +5,7 @@ export function ProfileImage() {
     <div className="relative">
       <div className="w-64 h-64 rounded-full overflow-hidden shadow-xl ring-4 ring-blue-100">
         <img
-          src="https://images-ext-1.discordapp.net/external/sQG27DntXDgCG9r-cTC--tjKx-Nl23mSfH8l2nms28E/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1199224091727626283/5295162a44afb0a65da57ec6c9dfaecd.png?format=webp&quality=lossless"
+          src="https://cdn.discordapp.com/avatars/796943270839451648/c47a2e6f39ba921ef9ca8b0a7cfaf32d.png?size=1024"
           alt="Profile"
           className="w-full h-full object-cover"
         />
